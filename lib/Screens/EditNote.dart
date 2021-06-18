9d3887ceb01b959dd7f03c8762d7106a2a95ef62
Arpit-Sahu <arpitsahu1911@gmial.com>
2021-06-18 10:38:46 +0530

    fixed scroll bug

--- lib/Screens/EditNote.dart
+++ lib/Screens/EditNote.dart
@@ -49,188 +49,190 @@ class _EditNoteState extends State<EditNote> {
       appBar: AppBar(
         title: Text('Edit Note'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+      body: SingleChildScrollView(
+              child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
 
-              Visibility(
-                visible: widget.hasVideo,
-                              child: Column(
-                  children: [
-                    
-                FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Center(
-                child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                ),
-            );
-          } else {
-            return Center(
-                child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-      SizedBox(
-        height: 10,
-      ),
-      ElevatedButton(
-        onPressed: () {
-          setState(() {
-            if (_controller.value.isPlaying) {
-                _controller.pause();
+                Visibility(
+                  visible: widget.hasVideo,
+                                child: Column(
+                    children: [
+                      
+                  FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
+              );
             } else {
-                _controller.play();
+              return Center(
+                  child: CircularProgressIndicator(),
+              );
             }
-          });
-        },
-        child:
-            Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
-      ),
-      SizedBox(
-        height: 50,
-      ),
-                  ],
-                ),
-              ),
-
-
-              Column(
-                children: [
-                TextFormField(
-                readOnly: widget.isTrash,
-                onTap: ()
-                {
-                  if(widget.isTrash)
-                    ScaffoldMessenger.of(context)
-                            .showSnackBar(snackBar('Cannot edit trash note'));
-                },
-                initialValue: widget.title,
-                onChanged: (value) {
-                  widget.title = value;
-                },
-                maxLength: 30,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  hintText: 'Title',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          },
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              if (_controller.value.isPlaying) {
+                  _controller.pause();
+              } else {
+                  _controller.play();
+              }
+            });
+          },
+          child:
+              Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                readOnly: widget.isTrash,
-                onTap: ()
-                {
-                  if(widget.isTrash)
-                    ScaffoldMessenger.of(context)
-                            .showSnackBar(snackBar('Cannot edit trash note'));
-                },
-                initialValue: widget.des,
-                onChanged: (value) {
-                  widget.des = value;
-                },
-                minLines: 1,
-                maxLines: 2,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  hintText: 'Description',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
+
+
+                Column(
                   children: [
-                    Checkbox(
-                    value: widget.hasVideo,
-                    onChanged: (val) {
-                      if(!widget.isTrash){
-                      setState(() {
-                        widget.hasVideo = val!;
-                      });
-                      }
-                    }),
-                    Text(
-                      'Video',
-                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                  TextFormField(
+                  readOnly: widget.isTrash,
+                  onTap: ()
+                  {
+                    if(widget.isTrash)
+                      ScaffoldMessenger.of(context)
+                              .showSnackBar(snackBar('Cannot edit trash note'));
+                  },
+                  initialValue: widget.title,
+                  onChanged: (value) {
+                    widget.title = value;
+                  },
+                  maxLength: 30,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     ),
-                  ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  readOnly: widget.isTrash,
+                  onTap: ()
+                  {
+                    if(widget.isTrash)
+                      ScaffoldMessenger.of(context)
+                              .showSnackBar(snackBar('Cannot edit trash note'));
+                  },
+                  initialValue: widget.des,
+                  onChanged: (value) {
+                    widget.des = value;
+                  },
+                  minLines: 1,
+                  maxLines: 2,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Description',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     ),
-                  Visibility(
-                    visible: widget.hasVideo,
-                      child: Flexible(
-                      child: TextFormField(
-                        initialValue: widget.videoLink,
-                        onChanged: (value) {
-                          widget.videoLink = value;
-                        },
-                        readOnly: widget.isTrash,
-                        minLines: 1,
-                        maxLines: 2,
-                        keyboardType: TextInputType.multiline,
-                        decoration: InputDecoration(
-                          hintText: 'Video Link',
-                          hintStyle: TextStyle(color: Colors.grey),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                    children: [
+                      Checkbox(
+                      value: widget.hasVideo,
+                      onChanged: (val) {
+                        if(!widget.isTrash){
+                        setState(() {
+                          widget.hasVideo = val!;
+                        });
+                        }
+                      }),
+                      Text(
+                        'Video',
+                        style: TextStyle(color: Colors.grey, fontSize: 12),
+                      ),
+                    ],
+                      ),
+                    Visibility(
+                      visible: widget.hasVideo,
+                        child: Flexible(
+                        child: TextFormField(
+                          initialValue: widget.videoLink,
+                          onChanged: (value) {
+                            widget.videoLink = value;
+                          },
+                          readOnly: widget.isTrash,
+                          minLines: 1,
+                          maxLines: 2,
+                          keyboardType: TextInputType.multiline,
+                          decoration: InputDecoration(
+                            hintText: 'Video Link',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Cancel'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print(widget.title);
-                      if (widget.title != '' && widget.title != null) {
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(snackBar('Saved!'));
-                        bloc.add(
-                            UpdateNoteEvent(id: widget.id!, title: widget.title!, des: widget.des!, hasVideo: widget.hasVideo, videoLink: widget.hasVideo? widget.videoLink!:''));
+                  ],
+                ),
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
                         Navigator.pop(context);
-                      } else {
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(snackBar('Title cannot be null'));
-                      }
-                    },
-                    child: Text('Save'),
-                  ),
-                ],
-              ),
-            
+                      },
+                      child: Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        print(widget.title);
+                        if (widget.title != '' && widget.title != null) {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(snackBar('Saved!'));
+                          bloc.add(
+                              UpdateNoteEvent(id: widget.id!, title: widget.title!, des: widget.des!, hasVideo: widget.hasVideo, videoLink: widget.hasVideo? widget.videoLink!:''));
+                          Navigator.pop(context);
+                        } else {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(snackBar('Title cannot be null'));
+                        }
+                      },
+                      child: Text('Save'),
+                    ),
+                  ],
+                ),
+              
 
-              ],),
-              ],
+                ],),
+                ],
+            ),
           ),
         ),
       ),
--- lib/Screens/NewNote.dart
+++ lib/Screens/NewNote.dart
@@ -25,123 +25,125 @@ class _NewNoteState extends State<NewNote> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                onChanged: (value) {
-                  title = value;
-                },
-                autofocus: true,
-                maxLength: 30,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  hintText: 'Title',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          child: SingleChildScrollView(
+                      child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  onChanged: (value) {
+                    title = value;
+                  },
+                  autofocus: true,
+                  maxLength: 30,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  description = value;
-                },
-                minLines: 1,
-                maxLines: 2,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  hintText: 'Description',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    description = value;
+                  },
+                  minLines: 1,
+                  maxLines: 2,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Description',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Checkbox(
-                          value: checked,
-                          onChanged: (val) {
-                            setState(() {
-                              checked = val!;
-                            });
-                          }),
-                      Text(
-                        'Video',
-                        style: TextStyle(color: Colors.grey, fontSize: 12),
-                      ),
-                    ],
-                  ),
-                  Visibility(
-                    visible: checked,
-                                      child: Flexible(
-                      child: TextFormField(
-                        onChanged: (value) {
-                          videoLink = value;
-                        },
-                        readOnly: !checked,
-                        minLines: 1,
-                        maxLines: 2,
-                        keyboardType: TextInputType.multiline,
-                        decoration: InputDecoration(
-                          hintText: 'Video Link',
-                          hintStyle: TextStyle(color: Colors.grey),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Checkbox(
+                            value: checked,
+                            onChanged: (val) {
+                              setState(() {
+                                checked = val!;
+                              });
+                            }),
+                        Text(
+                          'Video',
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    Visibility(
+                      visible: checked,
+                                        child: Flexible(
+                        child: TextFormField(
+                          onChanged: (value) {
+                            videoLink = value;
+                          },
+                          readOnly: !checked,
+                          minLines: 1,
+                          maxLines: 2,
+                          keyboardType: TextInputType.multiline,
+                          decoration: InputDecoration(
+                            hintText: 'Video Link',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Cancel'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      // if(!MyApp.isOnline)
-                      // {
-                      //   ScaffoldMessenger.of(context).showSnackBar(snackBar('No Internet'));
-                      //   return;
-                      // }
-                      if (title != '' && title != null) {
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar('Saved!'));
-                        bloc.add(CreateNewNotesEvent(
-                            hasVideo: checked,
-                            videoLink: checked?videoLink.toString():'null',
-                            title: title!, des: description!));
-                        bloc.add(NewNoteDB(title: title!, des: description!));
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
                         Navigator.pop(context);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar('Title cannot be null'));
-                        Navigator.pop(context);
-                      }
-                    },
-                    child: Text('Save'),
-                  ),
-                ],
-              ),
-            ],
+                      },
+                      child: Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        // if(!MyApp.isOnline)
+                        // {
+                        //   ScaffoldMessenger.of(context).showSnackBar(snackBar('No Internet'));
+                        //   return;
+                        // }
+                        if (title != '' && title != null) {
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar('Saved!'));
+                          bloc.add(CreateNewNotesEvent(
+                              hasVideo: checked,
+                              videoLink: checked?videoLink.toString():'null',
+                              title: title!, des: description!));
+                          bloc.add(NewNoteDB(title: title!, des: description!));
+                          Navigator.pop(context);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar('Title cannot be null'));
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text('Save'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
